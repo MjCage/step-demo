d015cf1c55a1966c8432475ce82215c7e0fc16dc
MjCage <michael@cage-dev.de>
2023-11-17 10:28:23 +0100

    feat: solana wallet connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between"></main>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <WalletMultiButton />
+    </main>
+  );
 }
---
+++ contexts/WalletProvider.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { FC, ReactNode, useMemo } from "react";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+
+// Default styles that can be overridden by your app
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+interface WalletContextProviderProps {
+  children?: ReactNode;
+}
+
+export const WalletContextProvider: FC<WalletContextProviderProps> = ({ children }) => {
+  const network = WalletAdapterNetwork.Mainnet;
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [new SolflareWalletAdapter(), new PhantomWalletAdapter()],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [network],
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
