f2a1a08b113a2307975e59e62b2e89134e80e3ee
MjCage <michael@cage-dev.de>
2023-11-20 17:27:26 +0100

    feat: notifications for connection state

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,19 @@
 import { ConnectScreen } from "@/components/screens/ConnectScreen";
 import { StakingScreen } from "@/components/screens/StakingScreen";
 import { useWallet } from "@solana/wallet-adapter-react";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
 
 export default function Home() {
   const { connected } = useWallet();
+
+  useEffect(() => {
+    if (connected) {
+      toast.success("Connected to wallet!");
+    } else {
+      toast.success("Disconnected from wallet!");
+    }
+  }, [connected]);
 
   return <main className="min-h-screen w-full">{connected ? <StakingScreen /> : <ConnectScreen />}</main>;
 }
